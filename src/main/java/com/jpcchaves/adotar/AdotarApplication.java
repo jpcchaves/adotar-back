38726e8c53deaa17c8c717cb6bc2c321602b5be5
João Paulo <jpcchaves@outlook.com>
2023-06-07 13:49:43 -0300

    first commit

---
+++ src/main/java/com/jpcchaves/adotar/AdotarApplication.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.adotar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpcchaves/adotar/AdotarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.adotar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
