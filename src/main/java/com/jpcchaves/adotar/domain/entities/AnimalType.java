f62ac63ec95dff9baf9872736ee478d380b62d0f
João Paulo <jpcchaves@outlook.com>
2023-06-07 22:25:06 -0300

    feat: add pojos for pet and animal type

---
+++ src/main/java/com/jpcchaves/adotar/domain/entities/AnimalType.java
@@ -0,0 +1,10 @@
+package com.jpcchaves.adotar.domain.entities;
+
+import java.util.Date;
+
+public class AnimalType {
+    private Long id;
+    private String type;
+    private Date createdAt;
+    private Date updatedAt;
+}
---
+++ src/main/java/com/jpcchaves/adotar/domain/entities/Pet.java
@@ -0,0 +1,12 @@
+package com.jpcchaves.adotar.domain.entities;
+
+import java.util.Date;
+
+public class Pet {
+    private Long id;
+    private String name;
+    private int age;
+    private boolean active;
+    private Date createdAt;
+    private Date updatedAt;
+}
