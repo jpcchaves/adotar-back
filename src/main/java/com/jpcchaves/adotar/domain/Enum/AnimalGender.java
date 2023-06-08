9b44c0ff38a7ac3a8d1e71b6719a673283520727
João Paulo <jpcchaves@outlook.com>
2023-06-08 13:20:09 -0300

    feat: add animal gender enum

---
+++ src/main/java/com/jpcchaves/adotar/domain/Enum/AnimalGender.java
@@ -0,0 +1,25 @@
+package com.jpcchaves.adotar.domain.Enum;
+
+public enum AnimalGender {
+    FEMALE('F'),
+    MALE('M');
+
+    private final char gender;
+
+    AnimalGender(char gender) {
+        this.gender = gender;
+    }
+
+    public static AnimalGender valueOf(char gender) {
+        for (AnimalGender value : AnimalGender.values()){
+            if(gender == value.getGender()) {
+                return value;
+            }
+        }
+        throw new  IllegalArgumentException("Invalid gender value: " + gender);
+    }
+
+    public char getGender() {
+        return gender;
+    }
+}
