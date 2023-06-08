e6ec61e64cea86146e2cc8f19e6cf5417acdf1ed
João Paulo <jpcchaves@outlook.com>
2023-06-08 16:11:19 -0300

    feat: add pet and animal type relation

--- src/main/java/com/jpcchaves/adotar/domain/entities/AnimalType.java
+++ src/main/java/com/jpcchaves/adotar/domain/entities/AnimalType.java
@@ -1,10 +1,66 @@
 package com.jpcchaves.adotar.domain.entities;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.util.Date;
 
+@Entity
+@Table(name = "animal_types")
 public class AnimalType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 30)
     private String type;
+    @CreationTimestamp
     private Date createdAt;
+    @UpdateTimestamp
     private Date updatedAt;
+
+    public AnimalType() {
+    }
+
+    public AnimalType(Long id,
+                      String type,
+                      Date createdAt,
+                      Date updatedAt) {
+        this.id = id;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/jpcchaves/adotar/domain/entities/Pet.java
+++ src/main/java/com/jpcchaves/adotar/domain/entities/Pet.java
@@ -43,11 +43,125 @@ public class Pet {
     )
     private Set<PetCharacteristic> characteristics = new HashSet<>();
 
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(
+            name = "type_id", referencedColumnName = "id"
+    )
+    private AnimalType type;
+
     @CreationTimestamp
     private Date createdAt;
     @UpdateTimestamp
     private Date updatedAt;
 
+    public Pet() {
+    }
+
+    public Pet(Long id,
+               String name,
+               int yearsAge,
+               int monthsAge,
+               char gender,
+               String color,
+               String description,
+               boolean active,
+               Set<PetCharacteristic> characteristics,
+               AnimalType type,
+               Date createdAt,
+               Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.yearsAge = yearsAge;
+        this.monthsAge = monthsAge;
+        this.gender = gender;
+        this.color = color;
+        this.description = description;
+        this.active = active;
+        this.characteristics = characteristics;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearsAge() {
+        return yearsAge;
+    }
+
+    public void setYearsAge(int yearsAge) {
+        this.yearsAge = yearsAge;
+    }
+
+    public int getMonthsAge() {
+        return monthsAge;
+    }
+
+    public void setMonthsAge(int monthsAge) {
+        this.monthsAge = monthsAge;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<PetCharacteristic> getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setCharacteristics(Set<PetCharacteristic> characteristics) {
+        this.characteristics = characteristics;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     public AnimalGender getGender() {
         return AnimalGender.valueOf(gender);
@@ -57,5 +171,13 @@ public class Pet {
         if (gender != null) {
             this.gender = gender.getGender();
         }
+    }
+
+    public AnimalType getType() {
+        return type;
+    }
+
+    public void setType(AnimalType type) {
+        this.type = type;
     }
 }
