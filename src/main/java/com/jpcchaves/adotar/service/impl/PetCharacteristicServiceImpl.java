d63bf3b622fd7f4fb198d352e7b79aba4e31094e
João Paulo <jpcchaves@outlook.com>
2023-06-08 16:19:41 -0300

    feat: add pet characteristics service interface and its impl

---
+++ src/main/java/com/jpcchaves/adotar/service/impl/PetCharacteristicServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jpcchaves.adotar.service.impl;
+
+import com.jpcchaves.adotar.service.usecases.PetCharacteristicService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetCharacteristicServiceImpl implements PetCharacteristicService {
+}
