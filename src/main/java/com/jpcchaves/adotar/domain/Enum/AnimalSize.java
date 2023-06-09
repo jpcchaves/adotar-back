bddd50a4f448953c72f297855eaa2c32eae1e43e
João Paulo <jpcchaves@outlook.com>
2023-06-09 16:24:44 -0300

    feat: add enum animal size

---
+++ src/main/java/com/jpcchaves/adotar/domain/Enum/AnimalSize.java
@@ -0,0 +1,27 @@
+package com.jpcchaves.adotar.domain.Enum;
+
+public enum AnimalSize {
+    TINY('T'),
+    SMALL('S'),
+    MEDIUM('M'),
+    LARGE('L');
+
+    private final char size;
+
+    AnimalSize(char size) {
+        this.size = size;
+    }
+
+    public static AnimalSize valueOf(char size) {
+        for (AnimalSize value : AnimalSize.values()) {
+            if (size == value.getSize()) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid gender value: " + size);
+    }
+
+    public char getSize() {
+        return size;
+    }
+}
