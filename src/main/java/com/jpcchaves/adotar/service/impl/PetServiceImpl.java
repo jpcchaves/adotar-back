ab68e9bc0650a6dd9d2df294b76ddb39e0e58558
João Paulo <jpcchaves@outlook.com>
2023-06-09 12:17:54 -0300

    feat: adjusted list all pets to return paginated response

--- src/main/java/com/jpcchaves/adotar/service/impl/PetServiceImpl.java
+++ src/main/java/com/jpcchaves/adotar/service/impl/PetServiceImpl.java
@@ -1,22 +1,40 @@
 package com.jpcchaves.adotar.service.impl;
 
 import com.jpcchaves.adotar.domain.entities.Pet;
+import com.jpcchaves.adotar.payload.dto.ApiResponsePaginatedDto;
+import com.jpcchaves.adotar.payload.dto.pet.PetDto;
 import com.jpcchaves.adotar.repository.PetRepository;
 import com.jpcchaves.adotar.service.usecases.PetService;
+import com.jpcchaves.adotar.utils.colletions.CollectionsUtils;
+import com.jpcchaves.adotar.utils.global.GlobalUtils;
+import com.jpcchaves.adotar.utils.mapper.MapperUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class PetServiceImpl implements PetService {
-   private final PetRepository petRepository;
+    private final PetRepository petRepository;
+    private final CollectionsUtils collectionUtils;
+    private final GlobalUtils globalUtils;
+    private final MapperUtils mapper;
 
-    public PetServiceImpl(PetRepository petRepository) {
+    public PetServiceImpl(PetRepository petRepository,
+                          CollectionsUtils collectionUtils,
+                          GlobalUtils globalUtils,
+                          MapperUtils mapper) {
         this.petRepository = petRepository;
+        this.collectionUtils = collectionUtils;
+        this.globalUtils = globalUtils;
+        this.mapper = mapper;
     }
 
     @Override
-    public List<Pet> listAll() {
-        return petRepository.findAll();
+    public ApiResponsePaginatedDto<PetDto> listAll(Pageable pageable) {
+        Page<Pet> petsPage = petRepository.findAll(pageable);
+
+        Page<PetDto> petDtoPage = globalUtils.buildPage(petsPage, pageable, PetDto.class);
+
+        return globalUtils.buildApiResponsePaginated(petDtoPage);
     }
 }
--- src/main/java/com/jpcchaves/adotar/service/usecases/PetService.java
+++ src/main/java/com/jpcchaves/adotar/service/usecases/PetService.java
@@ -1,9 +1,12 @@
 package com.jpcchaves.adotar.service.usecases;
 
 import com.jpcchaves.adotar.domain.entities.Pet;
+import com.jpcchaves.adotar.payload.dto.ApiResponsePaginatedDto;
+import com.jpcchaves.adotar.payload.dto.pet.PetDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PetService {
-    List<Pet> listAll();
+    ApiResponsePaginatedDto<PetDto> listAll(Pageable pageable);
 }
