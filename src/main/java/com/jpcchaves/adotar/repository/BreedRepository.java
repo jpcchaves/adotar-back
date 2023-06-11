f051f5991f4e5cd0a57dc35ad4d16575eed704b7
João Paulo <jpcchaves@outlook.com>
2023-06-11 19:32:34 -0300

    feat: add animaltype and breed repository

---
+++ src/main/java/com/jpcchaves/adotar/repository/BreedRepository.java
@@ -0,0 +1,9 @@
+package com.jpcchaves.adotar.repository;
+
+import com.jpcchaves.adotar.domain.entities.Breed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BreedRepository extends JpaRepository<Breed, Long> {
+}
