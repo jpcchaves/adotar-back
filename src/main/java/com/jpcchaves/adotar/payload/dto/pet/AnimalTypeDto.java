b0b51c2d3fcff06d934eccb5019313bbe8f35357
João Paulo <jpcchaves@outlook.com>
2023-06-11 17:29:31 -0300

    feat: add animaltype dto

--- src/main/java/com/jpcchaves/adotar/payload/dto/pet/AnimalTypeDto.java
+++ src/main/java/com/jpcchaves/adotar/payload/dto/pet/AnimalTypeDto.java
@@ -1,16 +1,22 @@
 package com.jpcchaves.adotar.payload.dto.pet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AnimalTypeDto {
     private Long id;
     private String type;
+    private Set<BreedDto> breeds = new HashSet<>();
 
     public AnimalTypeDto() {
     }
 
     public AnimalTypeDto(Long id,
-                         String type) {
+                         String type,
+                         Set<BreedDto> breeds) {
         this.id = id;
         this.type = type;
+        this.breeds = breeds;
     }
 
     public Long getId() {
@@ -27,5 +33,13 @@ public class AnimalTypeDto {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public Set<BreedDto> getBreeds() {
+        return breeds;
+    }
+
+    public void setBreeds(Set<BreedDto> breeds) {
+        this.breeds = breeds;
     }
 }
