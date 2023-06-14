2091a0a6d371703d442ae931d2e9ac2486f88807
João Paulo <jpcchaves@outlook.com>
2023-06-14 08:58:56 -0300

    refactor: remove unnecessary method and adjusted build api response paginated method to comport the removed unnecessary method

--- src/main/java/com/jpcchaves/adotar/utils/global/GlobalUtils.java
+++ src/main/java/com/jpcchaves/adotar/utils/global/GlobalUtils.java
@@ -1,43 +1,22 @@
 package com.jpcchaves.adotar.utils.global;
 
 import com.jpcchaves.adotar.payload.dto.ApiResponsePaginatedDto;
-import com.jpcchaves.adotar.utils.colletions.CollectionsUtils;
-import com.jpcchaves.adotar.utils.mapper.MapperUtils;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class GlobalUtils {
-
-    private final MapperUtils mapper;
-    private final CollectionsUtils collectionsUtils;
-
-    public GlobalUtils(MapperUtils mapper,
-                       CollectionsUtils collectionsUtils) {
-        this.mapper = mapper;
-        this.collectionsUtils = collectionsUtils;
-    }
-
-    public <T> ApiResponsePaginatedDto<T> buildApiResponsePaginated(Page<T> page) {
-        ApiResponsePaginatedDto<T> response = new ApiResponsePaginatedDto<>();
-        response.setContent(page.getContent());
+    public <T, D> ApiResponsePaginatedDto<D> buildApiResponsePaginated(Page<T> page,
+                                                                       List<D> content) {
+        ApiResponsePaginatedDto<D> response = new ApiResponsePaginatedDto<>();
+        response.setContent(content);
         response.setPageNo(page.getNumber());
         response.setPageSize(page.getSize());
         response.setTotalElements(page.getTotalElements());
         response.setTotalPages(page.getTotalPages());
         response.setLast(page.isLast());
         return response;
-    }
-
-
-    public <O, D> Page<D> buildPage(Page<O> pageOrigin,
-                                    Pageable pageable,
-                                    Class<D> destinationClass) {
-        List<D> destinationList = mapper.parseListObjects(pageOrigin.getContent(), destinationClass);
-        return new PageImpl<>(destinationList, pageable, destinationList.size());
     }
 }
