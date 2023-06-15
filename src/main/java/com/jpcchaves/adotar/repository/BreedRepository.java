0f8d6218f10f8fcb6572fd12a91ad17403086df6
João Paulo <jpcchaves@outlook.com>
2023-06-15 14:27:58 -0300

    feat: add new method in breed repository

--- src/main/java/com/jpcchaves/adotar/repository/BreedRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/BreedRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BreedRepository extends JpaRepository<Breed, Long> {
     List<Breed> findAllByAnimalType_Id(Long animalTypeId);
+
+    Optional<Breed> findByIdAndAnimalType_Id(Long id,
+                                             Long animalTypeId);
 }
