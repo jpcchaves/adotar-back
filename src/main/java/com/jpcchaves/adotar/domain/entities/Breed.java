98d51fa2bdd8de3c0bc876faa8234d65c2338fcb
João Paulo <jpcchaves@outlook.com>
2023-06-15 12:47:31 -0300

    refactor: adjusted breed name column definition

--- src/main/java/com/jpcchaves/adotar/domain/entities/Breed.java
+++ src/main/java/com/jpcchaves/adotar/domain/entities/Breed.java
@@ -8,7 +8,7 @@ public class Breed {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 50, nullable = false, unique = true)
+    @Column(length = 100, nullable = false, unique = true)
     private String name;
 
     @ManyToOne(
