bf1ed7fb720a9843105d0ad05b0db082991c3eef
João Paulo <jpcchaves@outlook.com>
2023-06-16 13:17:29 -0300

    feat: add customized response entity exception handler and add generic exception treatment

---
+++ src/main/java/com/jpcchaves/adotar/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.jpcchaves.adotar.exception.handler;
+
+import com.jpcchaves.adotar.exception.model.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    static Logger logger = Logger.getLogger("Exception");
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex,
+                                                                       WebRequest request) {
+        logger.severe("Error: " + ex.getClass() + " Message: " + ex.getMessage());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
