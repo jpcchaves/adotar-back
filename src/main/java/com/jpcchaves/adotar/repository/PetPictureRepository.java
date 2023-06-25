aaafeff5ac83488ce7377716e0bb9bda258b3647
João Paulo <jpcchaves@outlook.com>
2023-06-25 16:51:28 -0300

    feat: add get all pet pictures by pet id repository method

--- src/main/java/com/jpcchaves/adotar/repository/PetPictureRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/PetPictureRepository.java
@@ -4,6 +4,9 @@ import com.jpcchaves.adotar.domain.entities.PetPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PetPictureRepository extends JpaRepository<PetPicture, Long> {
+    List<PetPicture> getAllByPet_Id(Long petId);
 }
