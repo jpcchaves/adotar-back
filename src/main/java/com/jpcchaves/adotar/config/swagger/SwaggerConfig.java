ab142ccd0203adfb51a2ed9c5324a7b23b9b8aa0
João Paulo <jpcchaves@outlook.com>
2023-06-27 13:23:49 -0300

    feat: add swagger configuration

---
+++ src/main/java/com/jpcchaves/adotar/config/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.jpcchaves.adotar.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("Adotar - Rest API")
+                        .version("v1")
+                        .contact(new Contact().url("https://www.linkedin.com/in/joaopaulo-chaves/").email("jpcchaves@outlook.com"))
+                        .description("REST API built to manage a pet adoption application made with Spring Boot")
+                        .termsOfService("https://porfolio-jpcchaves.vercel.app/")
+                        .license(new License().name("MIT").url("https://porfolio-jpcchaves.vercel.app/"))
+        );
+    }
+}
