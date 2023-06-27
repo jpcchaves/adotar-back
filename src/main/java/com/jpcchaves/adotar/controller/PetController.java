110eca77fcadd1864d9d2585891cc75fb3d1be76
João Paulo <jpcchaves@outlook.com>
2023-06-27 14:07:13 -0300

    feat: add pet controller cors config

--- src/main/java/com/jpcchaves/adotar/controller/PetController.java
+++ src/main/java/com/jpcchaves/adotar/controller/PetController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/pets")
+@CrossOrigin(origins = "*")
 @Tag(name = "Pets-Controller")
 public class PetController {
     private final PetService petService;
