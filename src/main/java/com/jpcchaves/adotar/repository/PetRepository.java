a1ddea4e9479477401b29128fbc5bcc0684f1caf
João Paulo <jpcchaves@outlook.com>
2023-06-27 23:09:17 -0300

    feat: add pet repository methods to filter by animalType and/or Breed

--- src/main/java/com/jpcchaves/adotar/repository/PetRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/PetRepository.java
@@ -1,9 +1,20 @@
 package com.jpcchaves.adotar.repository;
 
 import com.jpcchaves.adotar.domain.entities.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    Page<Pet> getAllByBreed_Id(Pageable pageable,
+                               Long breedId);
+
+    Page<Pet> getAllByBreed_IdAndType_Id(Pageable pageable,
+                                         Long breedId,
+                                         Long typeId);
+
+    Page<Pet> getAllByType_Id(Pageable pageable,
+                              Long typeId);
 }
