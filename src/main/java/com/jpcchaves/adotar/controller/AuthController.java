b746eaf22d5b9528b480c4806a5da7218c0ebf9d
João Paulo <jpcchaves@outlook.com>
2023-06-28 16:17:17 -0300

    feat: add auth controller along with login, register and update user endpoints

---
+++ src/main/java/com/jpcchaves/adotar/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.jpcchaves.adotar.controller;
+
+import com.jpcchaves.adotar.payload.dto.ApiMessageResponseDto;
+import com.jpcchaves.adotar.payload.dto.auth.*;
+import com.jpcchaves.adotar.service.usecases.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<JwtAuthResponseDto> login(@Valid @RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(authService.login(loginDto));
+    }
+
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<RegisterResponseDto> register(@Valid @RequestBody RegisterRequestDto registerDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerDto));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiMessageResponseDto> update(@Valid @RequestBody UpdateUserRequestDto updateUserDto,
+                                                        @PathVariable(name = "id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.update(updateUserDto, id));
+    }
+}
