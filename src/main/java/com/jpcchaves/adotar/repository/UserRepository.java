f0f5099eb821334ac4da6af3720a9a030bbe7ed2
João Paulo <jpcchaves@outlook.com>
2023-06-28 13:54:54 -0300

    feat: add user and role repositories

---
+++ src/main/java/com/jpcchaves/adotar/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.jpcchaves.adotar.repository;
+
+import com.jpcchaves.adotar.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username,
+                                         String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
