bf62d66590f74f7214fa2860781a1d125cf45b3e
João Paulo <jpcchaves@outlook.com>
2023-06-30 14:05:54 -0300

    refactor: adjusted collection utils to be static

--- src/main/java/com/jpcchaves/adotar/utils/colletions/CollectionsUtils.java
+++ src/main/java/com/jpcchaves/adotar/utils/colletions/CollectionsUtils.java
@@ -1,22 +1,19 @@
 package com.jpcchaves.adotar.utils.colletions;
 
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Component
 public class CollectionsUtils {
     public CollectionsUtils() {
     }
 
-    public <T> List<T> convertSetToList(Set<T> objectsSet) {
+    public static <T> List<T> convertSetToList(Set<T> objectsSet) {
         return new ArrayList<>(objectsSet);
     }
 
-    public <T> Set<T> convertListToSet(List<T> objectsList) {
-       return new HashSet<>(objectsList);
+    public static <T> Set<T> convertListToSet(List<T> objectsList) {
+        return new HashSet<>(objectsList);
     }
 }
