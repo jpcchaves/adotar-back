78e67f9fe30c33591df1c0bbcee6ee6a03b424cd
João Paulo <jpcchaves@outlook.com>
2023-07-02 09:37:35 -0300

    feat: add state repository custom methods to find by name and uf

--- src/main/java/com/jpcchaves/adotar/repository/StateRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/StateRepository.java
@@ -4,6 +4,11 @@ import com.jpcchaves.adotar.domain.entities.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StateRepository extends JpaRepository<State, Long> {
+    Optional<State> findByName(String name);
+
+    Optional<State> findByUf(String uf);
 }
