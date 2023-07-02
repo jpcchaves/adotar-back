b277dd0f13abe14e8b2a8257764f256d89767852
João Paulo <jpcchaves@outlook.com>
2023-07-02 09:45:03 -0300

    feat: add state service and its implementation

---
+++ src/main/java/com/jpcchaves/adotar/service/impl/StateServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jpcchaves.adotar.service.impl;
+
+import com.jpcchaves.adotar.domain.entities.State;
+import com.jpcchaves.adotar.exception.ResourceNotFoundException;
+import com.jpcchaves.adotar.payload.dto.state.StateDto;
+import com.jpcchaves.adotar.repository.StateRepository;
+import com.jpcchaves.adotar.service.usecases.StateService;
+import com.jpcchaves.adotar.utils.mapper.MapperUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StateServiceImpl implements StateService {
+
+    private final StateRepository stateRepository;
+    private final MapperUtils mapperUtils;
+
+    public StateServiceImpl(StateRepository stateRepository,
+                            MapperUtils mapperUtils) {
+        this.stateRepository = stateRepository;
+        this.mapperUtils = mapperUtils;
+    }
+
+    @Override
+    public List<StateDto> getAll() {
+        List<State> states = stateRepository.findAll();
+        return mapperUtils.parseListObjects(states, StateDto.class);
+    }
+
+    @Override
+    public StateDto getByNameOrUf(String nameOrUf) {
+        State state = stateRepository
+                .findByNameOrUf(nameOrUf, nameOrUf)
+                .orElseThrow(() -> new ResourceNotFoundException("Estado não encontrado"));
+        return mapperUtils.parseObject(state, StateDto.class);
+    }
+}
---
+++ src/main/java/com/jpcchaves/adotar/service/usecases/StateService.java
@@ -0,0 +1,11 @@
+package com.jpcchaves.adotar.service.usecases;
+
+import com.jpcchaves.adotar.payload.dto.state.StateDto;
+
+import java.util.List;
+
+public interface StateService {
+    List<StateDto> getAll();
+
+    StateDto getByNameOrUf(String nameOrUf);
+}
