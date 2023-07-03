e94c42da0190798b468ea4c5b5095fd87dff8b5e
João Paulo <jpcchaves@outlook.com>
2023-07-03 15:06:27 -0300

    refactor: adjusted auth controller cors policy

--- src/main/java/com/jpcchaves/adotar/controller/AuthController.java
+++ src/main/java/com/jpcchaves/adotar/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin("*")
+@CrossOrigin
 public class AuthController {
     private final AuthService authService;
 
