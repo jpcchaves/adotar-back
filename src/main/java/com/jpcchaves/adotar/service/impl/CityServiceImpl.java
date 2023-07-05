43d06ac717134383012971be4e006e3fa1e2e3d4
João Paulo <jpcchaves@outlook.com>
2023-07-04 22:35:02 -0300

    refactor: adjusted implemented method to comport search by uf or state id

--- src/main/java/com/jpcchaves/adotar/service/impl/CityServiceImpl.java
+++ src/main/java/com/jpcchaves/adotar/service/impl/CityServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jpcchaves.adotar.service.impl;
 
 import com.jpcchaves.adotar.domain.entities.City;
+import com.jpcchaves.adotar.exception.BadRequestException;
 import com.jpcchaves.adotar.payload.dto.city.CityDto;
 import com.jpcchaves.adotar.repository.CityRepository;
 import com.jpcchaves.adotar.service.usecases.CityService;
@@ -22,13 +23,24 @@ public class CityServiceImpl implements CityService {
     }
 
     @Override
-    public List<CityDto> getAllCities(Long stateId) {
+    public List<CityDto> getAllCities(Long stateId,
+                                      String uf) {
         List<City> cities = new ArrayList<>();
 
-        if (stateId == null) {
+        if (stateId != null && uf != null) {
+            throw new BadRequestException("Informe apenas um parâmetro para realizar a busca");
+        }
+
+        if (stateId == null && uf == null) {
             cities.addAll(cityRepository.findAll());
-        } else {
+        }
+
+        if (stateId != null) {
             cities.addAll(cityRepository.findAllByState_Id(stateId));
+        }
+
+        if (stateId == null && uf != null) {
+            cities.addAll(cityRepository.findAllByState_UfIgnoreCase(uf));
         }
 
         return mapperUtils.parseListObjects(cities, CityDto.class);
