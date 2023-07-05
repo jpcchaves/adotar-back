468a77de1b37825545ff2bdbb978ab7244922101
João Paulo <jpcchaves@outlook.com>
2023-07-04 22:25:00 -0300

    feat: create city service interface

---
+++ src/main/java/com/jpcchaves/adotar/service/usecases/CityService.java
@@ -0,0 +1,10 @@
+package com.jpcchaves.adotar.service.usecases;
+
+import com.jpcchaves.adotar.payload.dto.city.CityDto;
+
+import java.util.List;
+
+public interface CityService {
+
+    List<CityDto> getAllCities(Long stateId);
+}
