3f978c8d74dee317cd2ffbeb94fb1d4f4c5114cd
João Paulo <jpcchaves@outlook.com>
2023-07-04 22:23:38 -0300

    refactor: remove get all cities by state method from state service

--- src/main/java/com/jpcchaves/adotar/service/impl/StateServiceImpl.java
+++ src/main/java/com/jpcchaves/adotar/service/impl/StateServiceImpl.java
@@ -1,11 +1,8 @@
 package com.jpcchaves.adotar.service.impl;
 
-import com.jpcchaves.adotar.domain.entities.City;
 import com.jpcchaves.adotar.domain.entities.State;
 import com.jpcchaves.adotar.exception.ResourceNotFoundException;
-import com.jpcchaves.adotar.payload.dto.city.CityDto;
 import com.jpcchaves.adotar.payload.dto.state.StateDto;
-import com.jpcchaves.adotar.repository.CityRepository;
 import com.jpcchaves.adotar.repository.StateRepository;
 import com.jpcchaves.adotar.service.usecases.StateService;
 import com.jpcchaves.adotar.utils.mapper.MapperUtils;
@@ -17,14 +14,11 @@ import java.util.List;
 public class StateServiceImpl implements StateService {
 
     private final StateRepository stateRepository;
-    private final CityRepository cityRepository;
     private final MapperUtils mapperUtils;
 
     public StateServiceImpl(StateRepository stateRepository,
-                            CityRepository cityRepository,
                             MapperUtils mapperUtils) {
         this.stateRepository = stateRepository;
-        this.cityRepository = cityRepository;
         this.mapperUtils = mapperUtils;
     }
 
@@ -42,9 +36,5 @@ public class StateServiceImpl implements StateService {
         return mapperUtils.parseObject(state, StateDto.class);
     }
 
-    @Override
-    public List<CityDto> getAllCitiesByState(Long stateId) {
-        List<City> cities = cityRepository.findAllByState_Id(stateId);
-        return mapperUtils.parseListObjects(cities, CityDto.class);
-    }
+
 }
--- src/main/java/com/jpcchaves/adotar/service/usecases/StateService.java
+++ src/main/java/com/jpcchaves/adotar/service/usecases/StateService.java
@@ -1,6 +1,5 @@
 package com.jpcchaves.adotar.service.usecases;
 
-import com.jpcchaves.adotar.payload.dto.city.CityDto;
 import com.jpcchaves.adotar.payload.dto.state.StateDto;
 
 import java.util.List;
@@ -10,5 +9,4 @@ public interface StateService {
 
     StateDto getByNameOrUf(String nameOrUf);
 
-    List<CityDto> getAllCitiesByState(Long stateId);
 }
