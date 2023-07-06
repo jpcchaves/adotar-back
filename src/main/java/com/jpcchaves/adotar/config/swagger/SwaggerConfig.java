81395bdaa6cbe4e7a13ff906eb7fc828ccb18bae
João Paulo <jpcchaves@outlook.com>
2023-07-05 22:31:56 -0300

    feat: add security requirement in swagger config

--- src/main/java/com/jpcchaves/adotar/config/swagger/SwaggerConfig.java
+++ src/main/java/com/jpcchaves/adotar/config/swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.jpcchaves.adotar.config.swagger;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,12 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @Configuration
 public class SwaggerConfig {
 
