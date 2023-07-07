a1205f263b643ff30ff667788807501b549b40d9
João Paulo <jpcchaves@outlook.com>
2023-07-07 12:34:18 -0300

    feat: add breed controller swagger documentation

--- src/main/java/com/jpcchaves/adotar/controller/BreedController.java
+++ src/main/java/com/jpcchaves/adotar/controller/BreedController.java
@@ -2,6 +2,13 @@ package com.jpcchaves.adotar.controller;
 
 import com.jpcchaves.adotar.payload.dto.pet.BreedDto;
 import com.jpcchaves.adotar.service.usecases.BreedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +21,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/breeds")
 @CrossOrigin("*")
+@Tag(name = "Breed-Controller")
+@SecurityRequirement(name = "Bearer Authentication")
 public class BreedController {
     private final BreedService breedService;
 
@@ -22,6 +31,26 @@ public class BreedController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Gets a list of breeds",
+            description = "Get a list of breeds by animal type (passing an animal type id)",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(
+                                            schema = @Schema(
+                                                    implementation = BreedDto.class
+                                            )
+                                    )
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<List<BreedDto>> findAllByAnimalType(@PathParam("animalTypeId") Long animalTypeId) {
         return ResponseEntity.ok(breedService.findAllByAnimalType(animalTypeId));
     }
