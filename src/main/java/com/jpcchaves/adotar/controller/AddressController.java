38f8ab48ffe6b4c69db259f784ef047f0805a2ec
João Paulo <jpcchaves@outlook.com>
2023-07-09 18:53:18 -0300

    feat: add address controller doc

--- src/main/java/com/jpcchaves/adotar/controller/AddressController.java
+++ src/main/java/com/jpcchaves/adotar/controller/AddressController.java
@@ -2,12 +2,20 @@ package com.jpcchaves.adotar.controller;
 
 import com.jpcchaves.adotar.payload.dto.address.AddressDto;
 import com.jpcchaves.adotar.service.usecases.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/addresses")
 @CrossOrigin
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "Address-Controller")
 public class AddressController {
     private final AddressService addressService;
 
@@ -15,11 +23,50 @@ public class AddressController {
         this.addressService = addressService;
     }
 
+    @Operation(
+            summary = "Get user's address",
+            description = "Get the current user's logged in address",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = AddressDto.class
+                                    )
+
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping
     public ResponseEntity<AddressDto> getUserAddress() {
         return ResponseEntity.ok(addressService.getUserAddress());
     }
 
+    @Operation(
+            summary = "Update user's address",
+            description = "Update the current user's logged in address",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = AddressDto.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PutMapping
     public ResponseEntity<AddressDto> updateUserAddress(@RequestBody AddressDto addressDto) {
         return ResponseEntity.ok(addressService.updateUserAddress(addressDto));
