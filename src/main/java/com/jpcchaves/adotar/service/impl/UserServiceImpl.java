3e258d48c8be7a70b9c971a2f48fd2c27dc1114f
João Paulo <jpcchaves@outlook.com>
2023-07-11 08:51:39 -0300

    feat: implemented user service

---
+++ src/main/java/com/jpcchaves/adotar/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.jpcchaves.adotar.service.impl;
+
+import com.jpcchaves.adotar.domain.entities.User;
+import com.jpcchaves.adotar.exception.UnexpectedErrorException;
+import com.jpcchaves.adotar.payload.dto.address.AddressDto;
+import com.jpcchaves.adotar.payload.dto.contact.ContactDto;
+import com.jpcchaves.adotar.payload.dto.user.UserDetailsDto;
+import com.jpcchaves.adotar.repository.UserRepository;
+import com.jpcchaves.adotar.service.usecases.SecurityContextService;
+import com.jpcchaves.adotar.service.usecases.UserService;
+import com.jpcchaves.adotar.utils.mapper.MapperUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final SecurityContextService securityContextService;
+    private final MapperUtils mapperUtils;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(SecurityContextService securityContextService,
+                           MapperUtils mapperUtils,
+                           UserRepository userRepository) {
+        this.securityContextService = securityContextService;
+        this.mapperUtils = mapperUtils;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetailsDto getUserDetails() {
+        User user = userRepository
+                .findById(securityContextService.getCurrentLoggedUser().getId())
+                .orElseThrow(() -> new UnexpectedErrorException("Ocorreu um erro inesperado, tente novamente"));
+
+        return buildUserDetails(user);
+    }
+
+    private UserDetailsDto buildUserDetails(User user) {
+        return new UserDetailsDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getEmail(),
+                mapperUtils.parseObject(user.getContact(), ContactDto.class),
+                mapperUtils.parseObject(user.getAddress(), AddressDto.class),
+                user.getLastSeen(),
+                user.getUpdatedAt(),
+                user.getCreatedAt()
+        );
+    }
+}
