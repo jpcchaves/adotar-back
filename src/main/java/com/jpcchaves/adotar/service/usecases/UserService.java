d1c2615a048e2db054c6109a639fcd10f517a609
João Paulo <jpcchaves@outlook.com>
2023-07-11 08:51:24 -0300

    feat: add user service interface along with its methods

---
+++ src/main/java/com/jpcchaves/adotar/service/usecases/UserService.java
@@ -0,0 +1,7 @@
+package com.jpcchaves.adotar.service.usecases;
+
+import com.jpcchaves.adotar.payload.dto.user.UserDetailsDto;
+
+public interface UserService {
+    UserDetailsDto getUserDetails();
+}
