895a22e54570e0a2445f3baf4752246c72c3e60f
João Paulo <jpcchaves@outlook.com>
2023-07-15 19:42:09 -0300

    refactor: update forgot-password public routes to allow all routes involved in forgot password process

--- src/main/java/com/jpcchaves/adotar/config/security/SecurityConfig.java
+++ src/main/java/com/jpcchaves/adotar/config/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers("/api/v1/auth/**", "/api/v1/forgot-password", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                                .requestMatchers("/api/v1/auth/**", "/api/v1/forgot-password/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
