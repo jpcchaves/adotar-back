ff958f1e6ca2f6d639bf666266add629048acdbb
João Paulo <jpcchaves@outlook.com>
2023-07-15 19:40:56 -0300

    feat: add new method findByToken

--- src/main/java/com/jpcchaves/adotar/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/PasswordResetTokenRepository.java
@@ -4,6 +4,9 @@ import com.jpcchaves.adotar.domain.entities.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
 }
--- src/main/java/com/jpcchaves/adotar/service/usecases/PasswordResetService.java
+++ src/main/java/com/jpcchaves/adotar/service/usecases/PasswordResetService.java
@@ -2,8 +2,11 @@ package com.jpcchaves.adotar.service.usecases;
 
 import com.jpcchaves.adotar.payload.dto.ApiMessageResponseDto;
 import com.jpcchaves.adotar.payload.dto.auth.PasswordResetRequestDto;
+import com.jpcchaves.adotar.payload.dto.auth.PasswordResetTokenRequestDto;
 import jakarta.mail.MessagingException;
 
 public interface PasswordResetService {
     ApiMessageResponseDto resetTokenRequestDto(PasswordResetRequestDto passwordResetRequestDto) throws MessagingException;
+
+    ApiMessageResponseDto resetPassword(PasswordResetTokenRequestDto passwordResetTokenRequestDto);
 }
