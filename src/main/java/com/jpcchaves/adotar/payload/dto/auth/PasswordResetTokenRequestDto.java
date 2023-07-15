6bd8d1d49d4e9658b97d644e8b7f32dc8afaecda
João Paulo <jpcchaves@outlook.com>
2023-07-15 19:49:46 -0300

    refactor: adjusted PasswordResetTokenRequestDto change email to token

--- src/main/java/com/jpcchaves/adotar/payload/dto/auth/PasswordResetTokenRequestDto.java
+++ src/main/java/com/jpcchaves/adotar/payload/dto/auth/PasswordResetTokenRequestDto.java
@@ -1,13 +1,12 @@
 package com.jpcchaves.adotar.payload.dto.auth;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public class PasswordResetTokenRequestDto {
-    @NotBlank(message = "Informe um email para continuar")
-    @Email(message = "Informe um email válido")
-    private String email;
+    @NotBlank(message = "O token é obrigatório")
+    @Length(min = 6, max = 6, message = "O token deve conter 6 caracteres")
+    private String token;
 
     @NotBlank(message = "A nova senha é obrigatória!")
     @Length(min = 6, message = "Senha deve conter pelo menos 6 caracteres!")
@@ -20,20 +19,20 @@ public class PasswordResetTokenRequestDto {
     public PasswordResetTokenRequestDto() {
     }
 
-    public PasswordResetTokenRequestDto(String email,
+    public PasswordResetTokenRequestDto(String token,
                                         String newPassword,
                                         String confirmNewPassword) {
-        this.email = email;
+        this.token = token;
         this.newPassword = newPassword;
         this.confirmNewPassword = confirmNewPassword;
     }
 
-    public String getEmail() {
-        return email;
+    public String getToken() {
+        return token;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setToken(String token) {
+        this.token = token;
     }
 
     public String getNewPassword() {
