06865d75621659f9be059d4a5df7ba52577eac7e
João Paulo <jpcchaves@outlook.com>
2023-07-16 13:34:13 -0300

    feat: add new method findByUser

--- src/main/java/com/jpcchaves/adotar/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/PasswordResetTokenRepository.java
@@ -1,6 +1,7 @@
 package com.jpcchaves.adotar.repository;
 
 import com.jpcchaves.adotar.domain.entities.PasswordResetToken;
+import com.jpcchaves.adotar.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+
+    Optional<PasswordResetToken> findByUser(User user);
 }
