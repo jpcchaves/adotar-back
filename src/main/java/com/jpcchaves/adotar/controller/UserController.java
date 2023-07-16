27ad398b0a89d15f95b932bcb78309db49ad795b
João Paulo <jpcchaves@outlook.com>
2023-07-16 13:01:14 -0300

    feat: add user controller doc

--- src/main/java/com/jpcchaves/adotar/controller/UserController.java
+++ src/main/java/com/jpcchaves/adotar/controller/UserController.java
@@ -2,6 +2,10 @@ package com.jpcchaves.adotar.controller;
 
 import com.jpcchaves.adotar.payload.dto.user.UserDetailsDto;
 import com.jpcchaves.adotar.service.usecases.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +26,27 @@ public class UserController {
         this.userService = userService;
     }
 
+
+    @Operation(
+            summary = "Get user all infos",
+            description = "Get the current user's logged in all infos",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = UserDetailsDto.class
+                                    )
+
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @GetMapping
     public ResponseEntity<UserDetailsDto> getUserDetails() {
         return ResponseEntity.ok(userService.getUserDetails());
