3a96b23cddf09fd929d1a1362ca3b85469bf8cc1
jpcchaves <jpcchaves@outlook.com>
2023-07-26 08:46:50 -0300

    feat: add new entity userSavedPets to manage database operations

---
+++ src/main/java/com/jpcchaves/adotar/domain/entities/UserSavedPets.java
@@ -0,0 +1,50 @@
+package com.jpcchaves.adotar.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_saved_pets")
+public class UserSavedPets {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    public UserSavedPets() {
+    }
+
+    public UserSavedPets(Long id, User user, Pet pet) {
+        this.id = id;
+        this.user = user;
+        this.pet = pet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
