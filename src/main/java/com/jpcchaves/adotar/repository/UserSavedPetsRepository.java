76cf9947dbb3cc1f005db5b4288695e84b72b451
jpcchaves <jpcchaves@outlook.com>
2023-07-27 14:40:46 -0300

    feat: add new jpa methods to find UserSavedPet by user and pet

--- src/main/java/com/jpcchaves/adotar/repository/UserSavedPetsRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/UserSavedPetsRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserSavedPetsRepository extends JpaRepository<UserSavedPets, Long> {
     List<UserSavedPets> findAllByUserId(Long userId);
+    Optional<UserSavedPets> findByPet_IdAndUser_Id(Long petId, Long userId);
+    boolean existsByPet_IdAndUser_Id(Long petId, Long userId);
 }
