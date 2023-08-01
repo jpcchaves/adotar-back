c9b56e87574d5cd8505a3a0ad260c0220cb69768
jpcchaves <jpcchaves@outlook.com>
2023-08-01 14:32:51 -0300

    feat: create petcard controller to handle pet card generation

---
+++ src/main/java/com/jpcchaves/adotar/controller/PetCardController.java
@@ -0,0 +1,30 @@
+package com.jpcchaves.adotar.controller;
+
+import com.jpcchaves.adotar.service.usecases.PetCardService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/pets/generate-card")
+public class PetCardController {
+    private final PetCardService petCardService;
+
+    public PetCardController(PetCardService petCardService) {
+        this.petCardService = petCardService;
+    }
+
+    @GetMapping("/{petId}")
+    public ResponseEntity<byte[]> generatePetCard(@PathVariable(name = "petId") Long petId) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        return new ResponseEntity<>(petCardService.generatePetCard(petId), headers, HttpStatus.OK);
+    }
+}
