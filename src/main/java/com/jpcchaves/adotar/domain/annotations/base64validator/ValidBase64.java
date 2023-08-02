0f2a28743ffbd9137e00297ca4ffb247a00ca733
jpcchaves <jpcchaves@outlook.com>
2023-08-02 15:14:22 -0300

    refactor: adjusted validbase64 error message

--- src/main/java/com/jpcchaves/adotar/domain/annotations/base64validator/ValidBase64.java
+++ src/main/java/com/jpcchaves/adotar/domain/annotations/base64validator/ValidBase64.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidBase64 {
-    String message() default "O base64 é inválido!";
+    String message() default "O formato do Base64 informado é inválido! Verifique o valor informado e tente novamente. Certifique-se de enviar o Base64 com prefixo.";
 
 
     Class<?>[] groups() default {};
