6ed956489325b59f4b2fd2830589894b7b4884b1
João Paulo <jpcchaves@outlook.com>
2023-08-06 19:30:21 -0300

    feat: add usermindto pojo class

---
+++ src/main/java/com/jpcchaves/adotar/payload/dto/user/UserMinDto.java
@@ -0,0 +1,75 @@
+package com.jpcchaves.adotar.payload.dto.user;
+
+public class UserMinDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String name;
+    private String username;
+    private String email;
+
+    public UserMinDto() {
+    }
+
+    public UserMinDto(Long id,
+                      String firstName,
+                      String lastName,
+                      String name,
+                      String username,
+                      String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
