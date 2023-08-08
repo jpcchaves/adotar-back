5406d5cfe63531ed840df8bae87666f4b7f52d8e
João Paulo <jpcchaves@outlook.com>
2023-08-07 22:12:26 -0300

    feat: add pet characteristics service interface

--- src/main/java/com/jpcchaves/adotar/service/usecases/PetCharacteristicService.java
+++ src/main/java/com/jpcchaves/adotar/service/usecases/PetCharacteristicService.java
@@ -1,4 +1,9 @@
 package com.jpcchaves.adotar.service.usecases;
 
+import com.jpcchaves.adotar.payload.dto.pet.PetCharacteristicsDto;
+
+import java.util.List;
+
 public interface PetCharacteristicService {
+    List<PetCharacteristicsDto> getAll();
 }
