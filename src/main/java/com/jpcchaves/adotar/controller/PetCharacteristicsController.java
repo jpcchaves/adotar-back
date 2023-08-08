49d50cf9f36f508f4d84d9893aa3baa8916e5fd4
João Paulo <jpcchaves@outlook.com>
2023-08-07 22:15:41 -0300

    feat: add pet characteristic controller documentation

--- src/main/java/com/jpcchaves/adotar/controller/PetCharacteristicsController.java
+++ src/main/java/com/jpcchaves/adotar/controller/PetCharacteristicsController.java
@@ -2,6 +2,11 @@ package com.jpcchaves.adotar.controller;
 
 import com.jpcchaves.adotar.payload.dto.pet.PetCharacteristicsDto;
 import com.jpcchaves.adotar.service.usecases.PetCharacteristicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,6 +28,26 @@ public class PetCharacteristicsController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Gets a list of pet characteristics",
+            description = "Get a list of pet characteristics",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(
+                                    array = @ArraySchema(
+                                            schema = @Schema(
+                                                    implementation = PetCharacteristicsDto.class
+                                            )
+                                    )
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<List<PetCharacteristicsDto>> getAll() {
         return ResponseEntity.ok(petCharacteristicService.getAll());
     }
