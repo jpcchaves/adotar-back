9e54334868d0104dab275b9198524405bda7a2f2
jpcchaves <jpcchaves@outlook.com>
2023-08-09 13:54:27 -0300

    feat: add sendContactMessage usecase in email service

--- src/main/java/com/jpcchaves/adotar/service/usecases/EmailService.java
+++ src/main/java/com/jpcchaves/adotar/service/usecases/EmailService.java
@@ -1,8 +1,12 @@
 package com.jpcchaves.adotar.service.usecases;
 
 import com.jpcchaves.adotar.domain.entities.PasswordResetToken;
+import com.jpcchaves.adotar.payload.dto.ApiMessageResponseDto;
+import com.jpcchaves.adotar.payload.dto.email.ContactEmailDto;
 import jakarta.mail.MessagingException;
 
 public interface EmailService {
-    void sendPasswordRequest(PasswordResetToken passwordResetRequestDto) throws MessagingException;
+    void sendResetPasswordRequest(PasswordResetToken passwordResetRequestDto) throws MessagingException;
+
+    ApiMessageResponseDto sendContactMessage(ContactEmailDto contactEmailDto) throws MessagingException;
 }
