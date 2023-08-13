a39148eac7d1d1547e37235ae8688e3f51345960
João Paulo <jpcchaves@outlook.com>
2023-08-13 10:54:51 -0300

    feat: create enum user roles

---
+++ src/main/java/com/jpcchaves/adotar/domain/Enum/UserRoles.java
@@ -0,0 +1,16 @@
+package com.jpcchaves.adotar.domain.Enum;
+
+public enum UserRoles {
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private final String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
