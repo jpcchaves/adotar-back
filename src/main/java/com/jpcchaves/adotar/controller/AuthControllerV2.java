456983802257ff12820b919a42433ccc8e4d7f42
João Paulo <jpcchaves@outlook.com>
2023-10-17 21:52:48 -0300

    feat: endpoint v2 to login

---
+++ src/main/java/com/jpcchaves/adotar/controller/AuthControllerV2.java
@@ -0,0 +1,26 @@
+package com.jpcchaves.adotar.controller;
+
+import com.jpcchaves.adotar.payload.dto.auth.JwtAuthResponseDto;
+import com.jpcchaves.adotar.payload.dto.auth.LoginDtoV2;
+import com.jpcchaves.adotar.service.usecases.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v2/auth")
+@CrossOrigin
+@Tag(name = "Auth-Controller")
+public class AuthControllerV2 {
+    private final AuthService authService;
+
+    public AuthControllerV2(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<JwtAuthResponseDto> loginV2(@Valid @RequestBody LoginDtoV2 loginDtoV2) {
+        return ResponseEntity.ok(authService.loginV2(loginDtoV2));
+    }
+}
