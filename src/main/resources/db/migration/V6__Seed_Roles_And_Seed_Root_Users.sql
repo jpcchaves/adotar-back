3908093921dfa10ed97c4e42d4781b64f1f3d8a4
jpcchaves <joao.chaves@liferay.com>
2023-10-24 11:55:25 -0300

    refactor: adjustments in migration V6 to change passwords of root users

--- src/main/resources/db/migration/V6__Seed_Roles_And_Seed_Root_Users.sql
+++ src/main/resources/db/migration/V6__Seed_Roles_And_Seed_Root_Users.sql
@@ -5,9 +5,9 @@ values ('ROLE_ADMIN'),
 insert into "public"."users" ("created_at", "deleted_at", "email", "first_name", "is_active", "is_admin",
                               "last_name", "password", "updated_at", "username")
 values ('2023-06-28', DEFAULT, 'adotar@admin.com', 'Admin', true, true, 'Adotar',
-        '$2a$12$0Ds..BFWJXmd0RH10IvdDuZRwYtIBZCDBiO/dpyp4.Qme7k7dtJ0a', '2023-06-28', 'admin'),
+        '$2a$12$dESCyPL02lbPBH4Mt8Pc5.iwmq/Gmw6fAh/i1RukJG76aS0MMbDz6', '2023-06-28', 'admin'),
        ('2023-06-28 00:00:00', NULL, 'root@root.com', 'Root', true, false, 'Root',
-        '$2a$08$p0aZ7qiVTJJ8kZiJcaivPOQbkS.tsnh/oMZ6mV.NRjH6fkb69kbC2',
+        '$2a$12$dESCyPL02lbPBH4Mt8Pc5.iwmq/Gmw6fAh/i1RukJG76aS0MMbDz6',
         '2023-06-28 00:00:00', 'root');
 
 insert into "public"."users_roles" ("role_id", "user_id")
