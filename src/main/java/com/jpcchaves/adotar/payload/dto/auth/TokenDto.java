a5b29725875e6f9a47eb34bc779e7913c771e94e
jpcchaves <joao.chaves@liferay.com>
2023-11-09 11:10:30 -0300

    feat: add pojo class for token dto

---
+++ src/main/java/com/jpcchaves/adotar/payload/dto/auth/TokenDto.java
@@ -0,0 +1,20 @@
+package com.jpcchaves.adotar.payload.dto.auth;
+
+public class TokenDto {
+    private String accessToken;
+
+    public TokenDto() {
+    }
+
+    public TokenDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
