8dd1e3b9b74217b81ccd9b362dcce0e88889d2d6
jpcchaves <joao.chaves@liferay.com>
2023-11-22 13:27:55 -0300

    feat: create method to filter pets by a given list of animal types ids

--- src/main/java/com/jpcchaves/adotar/repository/PetRepository.java
+++ src/main/java/com/jpcchaves/adotar/repository/PetRepository.java
@@ -4,8 +4,11 @@ import com.jpcchaves.adotar.domain.entities.Pet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,6 +19,10 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
 
     Page<Pet> getAllByType_Id(Pageable pageable,
                               Long typeId);
+
+    @Query("SELECT p FROM Pet p WHERE p.type.id IN :typeIds")
+    Page<Pet> findByTypes(Pageable pageable,
+                          @Param("typeIds") List<Long> typeIds);
 
     Page<Pet> getAllByUser_Id(Pageable pageable,
                               Long userId);
