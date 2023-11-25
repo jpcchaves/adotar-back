3d4a7e998c63426ba0eeddb43922f8d95aeddd0d
João Paulo <jpcchaves@outlook.com>
2023-11-25 11:32:40 -0300

    feat: create dto method to user picture

---
+++ src/main/java/com/jpcchaves/adotar/payload/dto/user/UserPictureDto.java
@@ -0,0 +1,23 @@
+package com.jpcchaves.adotar.payload.dto.user;
+
+import com.jpcchaves.adotar.domain.annotations.base64validator.ValidBase64;
+
+public class UserPictureDto {
+    @ValidBase64
+    private String pictureUrl;
+
+    public UserPictureDto() {
+    }
+
+    public UserPictureDto(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
