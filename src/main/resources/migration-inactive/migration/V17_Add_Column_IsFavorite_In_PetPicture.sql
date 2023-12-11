e2e023f23040453449463501f18a13c61c2a3d9e
jpcchaves <joao.chaves@liferay.com>
2023-12-11 08:53:29 -0300

    refactor: removing unused deprecated migrations

--- src/main/resources/migration-inactive/migration/V17_Add_Column_IsFavorite_In_PetPicture.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "public"."pet_picture"
-    ADD COLUMN "is_favorite" boolean NOT NULL;
--- src/main/resources/migration-inactive/migration/V3__Create_Table_Pets_Characteristics.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE
-    public.pets_characteristics
-(
-    pet_id            bigint NOT NULL,
-    characteristic_id bigint NOT NULL
-);
