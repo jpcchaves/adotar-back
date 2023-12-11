498e4acd1c09621adaa04c1a03108b1dec800400
jpcchaves <joao.chaves@liferay.com>
2023-12-11 09:33:57 -0300

    refactor: adjusting breed name column, so it can receive can duplicate names and adjusted column length to improve db optimization

--- src/main/java/com/jpcchaves/adotar/domain/entities/Breed.java
+++ src/main/java/com/jpcchaves/adotar/domain/entities/Breed.java
@@ -11,7 +11,7 @@ public class Breed {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 100, nullable = false, unique = true)
+    @Column(length = 60, nullable = false)
     private String name;
 
     @ManyToOne(
