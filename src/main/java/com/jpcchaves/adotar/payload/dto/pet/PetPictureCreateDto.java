7c04bb30c56c535287e80593e001cfeb8cbe3ecc
João Chaves <joao.chaves@liferay.com>
2024-01-06 16:38:11 -0300

    feat: add pet picture create dto

--- src/main/java/com/jpcchaves/adotar/payload/dto/pet/PetPictureCreateDto.java
+++ src/main/java/com/jpcchaves/adotar/payload/dto/pet/PetPictureCreateDto.java
@@ -1,2 +1,20 @@
-package com.jpcchaves.adotar.payload.dto.pet;public class PetPictureCreateDto {
+package com.jpcchaves.adotar.payload.dto.pet;
+
+public class PetPictureCreateDto {
+    private String imgUrl;
+
+    public PetPictureCreateDto() {
+    }
+
+    public PetPictureCreateDto(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 }
