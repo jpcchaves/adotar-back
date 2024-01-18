6f628aa0ae30e96a1f55cd4bfa9c64890293dea9
jpcchaves <joao.chaves@liferay.com>
2024-01-18 16:55:26 -0300

    feat: add pet picture entity

---
+++ src/main/java/com/jpcchaves/adotar/domain/entities/PetPicture.java
@@ -0,0 +1,68 @@
+package com.jpcchaves.adotar.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class PetPicture {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private long size;
+
+    @Column(nullable = false)
+    private String type;
+
+    public PetPicture() {
+    }
+
+    public PetPicture(UUID id,
+                      String name,
+                      long size,
+                      String type) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
