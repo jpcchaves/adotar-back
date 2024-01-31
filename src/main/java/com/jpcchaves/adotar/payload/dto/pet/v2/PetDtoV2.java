8273a58b12527536b420c8bc92b51316b6ab7f25
João Chaves <joao.chaves@liferay.com>
2024-01-30 21:19:26 -0300

    feat: add address in get pet by id

--- src/main/java/com/jpcchaves/adotar/payload/dto/pet/v2/PetDtoV2.java
+++ src/main/java/com/jpcchaves/adotar/payload/dto/pet/v2/PetDtoV2.java
@@ -5,6 +5,7 @@ import com.jpcchaves.adotar.domain.Enum.AnimalSize;
 import com.jpcchaves.adotar.domain.Enum.HealthCondition;
 import com.jpcchaves.adotar.domain.entities.AnimalType;
 import com.jpcchaves.adotar.domain.entities.Breed;
+import com.jpcchaves.adotar.payload.dto.address.AddressMinDto;
 import com.jpcchaves.adotar.payload.dto.pet.AnimalTypeDto;
 import com.jpcchaves.adotar.payload.dto.pet.BreedDto;
 import com.jpcchaves.adotar.payload.dto.pet.PetCharacteristicsDto;
@@ -31,6 +32,7 @@ public class PetDtoV2 {
     private String type;
     private String breed;
     private List<PetPictureDto> petPictures = new ArrayList<>();
+    private AddressMinDto address;
 
     public PetDtoV2() {
     }
@@ -52,7 +54,8 @@ public class PetDtoV2 {
                     Set<PetCharacteristicsDto> characteristics,
                     AnimalTypeDto type,
                     BreedDto breed,
-                    List<PetPictureDto> petPictures) {
+                    List<PetPictureDto> petPictures,
+                    AddressMinDto address) {
         this.id = id;
         this.name = name;
         this.yearsAge = yearsAge;
@@ -71,6 +74,7 @@ public class PetDtoV2 {
         this.type = type.getType();
         this.breed = breed.getName();
         this.petPictures = petPictures;
+        this.address = address;
     }
 
 
@@ -220,5 +224,13 @@ public class PetDtoV2 {
 
     public void setPetPictures(List<PetPictureDto> petPictures) {
         this.petPictures = petPictures;
+    }
+
+    public AddressMinDto getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressMinDto address) {
+        this.address = address;
     }
 }
