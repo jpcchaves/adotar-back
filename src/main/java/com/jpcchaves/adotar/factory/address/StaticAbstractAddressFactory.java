543a21524ff6891df096011b0dddec8b1108d68e
jpcchaves <joao.chaves@liferay.com>
2024-03-19 09:47:32 -0300

    feat: add static abstract address factory

---
+++ src/main/java/com/jpcchaves/adotar/factory/address/StaticAbstractAddressFactory.java
@@ -0,0 +1,9 @@
+package com.jpcchaves.adotar.factory.address;
+
+import com.jpcchaves.adotar.application.dto.address.AddressResponseDto;
+
+public abstract class StaticAbstractAddressFactory {
+    public static AddressResponseDto createAddressResponseDtoWithDefaultValues() {
+        return new AddressResponseDto();
+    }
+}
