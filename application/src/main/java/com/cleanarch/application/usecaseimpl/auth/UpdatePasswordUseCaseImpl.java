fd93f6185accd71178d6b7cee39ccb9dacd25417
Joao Chaves <joao.chaves@liferay.com>
2024-07-03 17:01:28 -0300

    add update password gateway and usecase impl

---
+++ application/src/main/java/com/cleanarch/application/usecaseimpl/auth/UpdatePasswordUseCaseImpl.java
@@ -0,0 +1,21 @@
+package com.cleanarch.application.usecaseimpl.auth;
+
+import com.cleanarch.application.gateway.auth.*;
+import com.cleanarch.usecase.auth.*;
+import com.cleanarch.usecase.auth.dto.*;
+import com.cleanarch.usecase.dto.*;
+
+public class UpdatePasswordUseCaseImpl implements UpdatePasswordUseCase {
+
+  private final UpdatePasswordGateway updatePasswordGateway;
+
+  public UpdatePasswordUseCaseImpl(UpdatePasswordGateway updatePasswordGateway) {
+    this.updatePasswordGateway = updatePasswordGateway;
+  }
+
+  @Override
+  public MessageResponseDTO updatePassword(UpdatePasswordRequestDTO requestDTO) {
+
+    return updatePasswordGateway.updatePassword(requestDTO);
+  }
+}
