32943ea7a0487ef0031145e1259581a68c026fa1
Joao Chaves <joao.chaves@liferay.com>
2024-07-04 08:50:03 -0300

    add pet characteristics constant

---
+++ core/src/main/java/br/com/jpcchaves/core/domain/constants/PetCharacteristics.java
@@ -0,0 +1,39 @@
+package br.com.jpcchaves.core.domain.constants;
+
+import java.util.*;
+
+public abstract class PetCharacteristics {
+  private static final List<String> PET_CHARACTERISTICS =
+      Collections.unmodifiableList(
+          List.of(
+              "Carinhoso",
+              "Amigável",
+              "Leal",
+              "Brincalhão",
+              "Protetor",
+              "Dócil",
+              "Inteligente",
+              "Obediente",
+              "Curioso",
+              "Energético",
+              "Companheiro",
+              "Silencioso",
+              "Independente",
+              "Sociável",
+              "Atento",
+              "Esperto",
+              "Divertido",
+              "Calmo",
+              "Fofinho",
+              "Disciplinado",
+              "Paciente",
+              "Adaptável",
+              "Ágil",
+              "Curioso",
+              "Manso",
+              "Afetuoso",
+              "Corajoso",
+              "Vigilante",
+              "Tranquilo",
+              "Afável"));
+}
