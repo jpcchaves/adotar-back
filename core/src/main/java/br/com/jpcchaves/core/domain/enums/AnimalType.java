0ce9ccf05411c11f28e7455dbc282991a373b8ff
João Chaves <joao.chaves@liferay.com>
2024-07-05 20:54:09 -0300

    add animal type enum

--- core/src/main/java/br/com/jpcchaves/core/domain/enums/AnimalType.java
+++ core/src/main/java/br/com/jpcchaves/core/domain/enums/AnimalType.java
@@ -1,4 +1,39 @@
 package br.com.jpcchaves.core.domain.enums;
 
+import java.util.Objects;
+
 public enum AnimalType {
+  DOG("D"),
+  CAT("C"),
+  BIRD("B");
+
+  private final String animalType;
+
+  AnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+
+  private static String fromValue(String animalType) {
+
+    for (AnimalType type : values()) {
+
+      if (Objects.equals(type.animalType, animalType)) {
+
+        return animalType;
+      }
+    }
+
+    throw new IllegalArgumentException("Invalid animal type!");
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  @Override
+  public String toString() {
+    return "AnimalType{" +
+        "animalType='" + animalType + '\'' +
+        '}';
+  }
 }
