a00c1269bd26697a79f3cff48974b2d5f4f06d77
João Chaves <joao.chaves@liferay.com>
2024-07-07 14:15:21 -0300

    adjust naming of use case

--- usecase/src/main/java/com/cleanarch/usecase/pet/AddUserFavoritePet.java
+++
@@ -1,8 +0,0 @@
-package com.cleanarch.usecase.pet;
-
-import com.cleanarch.usecase.common.dto.MessageResponseDTO;
-
-public interface AddUserFavoritePet {
-
-  MessageResponseDTO add(Long petId);
-}
--- usecase/src/main/java/com/cleanarch/usecase/pet/RemoveUserFavoritePet.java
+++
@@ -1,8 +0,0 @@
-package com.cleanarch.usecase.pet;
-
-import com.cleanarch.usecase.common.dto.MessageResponseDTO;
-
-public interface RemoveUserFavoritePet {
-
-  MessageResponseDTO remove(Long petId);
-}
