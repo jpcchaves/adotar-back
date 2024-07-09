134e8e0e361d2d34b59229325cfcf155c07df1e0
João Chaves <joao.chaves@liferay.com>
2024-07-09 19:08:00 -0300

    add infra module that will replace the current one

---
+++ infra/src/main/java/com/cleanarch/infra/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package com.cleanarch.infra;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationRunner.class, args);
+  }
+}
