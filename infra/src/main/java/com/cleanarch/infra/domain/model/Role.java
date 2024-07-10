5efb961863261c7ab7dd75e8774a60675c865c95
Joao Chaves <joao.chaves@liferay.com>
2024-07-10 16:35:48 -0300

    add role entity implementing granted authority

---
+++ infra/src/main/java/com/cleanarch/infra/domain/model/Role.java
@@ -0,0 +1,54 @@
+package com.cleanarch.infra.domain.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.*;
+
+import java.io.*;
+
+@Entity
+@Table(name = "roles")
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1)
+public class Role implements GrantedAuthority, Serializable {
+
+  @Serial
+  private static final long serialVersionUID = -4099401606873221148L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+  private Long id;
+
+  @Column(nullable = false, length = 50)
+  private String name;
+
+  public Role() {
+  }
+
+  public Role(
+      Long id,
+      String name
+  ) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String getAuthority() {
+    return this.name;
+  }
+}
