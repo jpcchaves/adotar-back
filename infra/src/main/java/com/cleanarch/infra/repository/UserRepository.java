e65baeb260a45ecd8b97ea3e56b58e9f2f8cc025
Joao Chaves <joao.chaves@liferay.com>
2024-07-11 16:39:31 -0300

    add user jpa repository along with custom methods

---
+++ infra/src/main/java/com/cleanarch/infra/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.cleanarch.infra.repository;
+
+import com.cleanarch.infra.domain.model.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+  @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
+  Optional<User> findByEmail(@Param("email") String email);
+
+  @Query(value = "SELECT EXISTS(SELECT 1 FROM users u WHERE u.email = :email)", nativeQuery = true)
+  Boolean existsByEmail(@Param("email") String email);
+}
