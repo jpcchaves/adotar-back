05fd125440894ae872ef6f85b982e7cdaaf0cdb9
João Chaves <joao.chaves@liferay.com>
2024-07-13 17:47:07 -0300

    add logger for both access denied handle impl and authentication entrypoint impl

--- infra/src/main/java/com/cleanarch/infra/config/security/AccessDeniedHandlerImpl.java
+++ infra/src/main/java/com/cleanarch/infra/config/security/AccessDeniedHandlerImpl.java
@@ -7,12 +7,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+
+  private static final Logger logger = LoggerFactory.getLogger(
+      AccessDeniedHandlerImpl.class);
 
   @Override
   public void handle(
@@ -39,5 +44,8 @@ public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
                 .writer()
                 .writeValueAsString(exceptionResponseDTO)
         );
+
+    logger.error("Error in class: " + this.getClass().getSimpleName() + ". "
+        + exceptionResponseDTO);
   }
 }
