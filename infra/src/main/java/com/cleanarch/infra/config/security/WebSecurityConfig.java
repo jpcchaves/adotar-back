6e9c30f94720051539909267940461787408811f
João Chaves <joao.chaves@liferay.com>
2024-07-13 17:17:58 -0300

    add logger in web security config

--- infra/src/main/java/com/cleanarch/infra/config/security/WebSecurityConfig.java
+++ infra/src/main/java/com/cleanarch/infra/config/security/WebSecurityConfig.java
@@ -1,52 +1,91 @@
 package com.cleanarch.infra.config.security;
 
-import org.springframework.context.annotation.*;
-import org.springframework.security.authentication.*;
-import org.springframework.security.config.*;
-import org.springframework.security.config.annotation.authentication.configuration.*;
-import org.springframework.security.config.annotation.method.configuration.*;
-import org.springframework.security.config.annotation.web.builders.*;
-import org.springframework.security.config.annotation.web.configurers.*;
-import org.springframework.security.config.http.*;
-import org.springframework.security.web.*;
+import br.com.jpcchaves.core.exception.InternalServerError;
+import br.com.jpcchaves.core.exception.enums.ExceptionDefinition;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
+  private static final Logger logger = LoggerFactory.getLogger(
+      WebSecurityConfig.class);
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+  }
+
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+  public static PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+      throws Exception {
     return configuration.getAuthenticationManager();
   }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)
+      throws Exception {
+    try {
+      http
+          .formLogin(AbstractHttpConfigurer::disable)
+          .httpBasic(AbstractHttpConfigurer::disable)
+          .csrf(AbstractHttpConfigurer::disable)
+          .headers(
+              (headers) ->
+                  headers
+                      .contentTypeOptions(Customizer.withDefaults())
+                      .xssProtection(Customizer.withDefaults())
+                      .cacheControl(Customizer.withDefaults())
+                      .httpStrictTransportSecurity(Customizer.withDefaults())
+                      .frameOptions(Customizer.withDefaults())
+                      .disable())
+          .authorizeHttpRequests(
+              authorize ->
+                  authorize
+                      .requestMatchers(
+                          "/h2/**",
+                          "/h2-console/**")
+                      .permitAll()
+                      .anyRequest()
+                      .authenticated()
+          )
+          .sessionManagement(session ->
+              session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          )
+          .cors(Customizer.withDefaults());
 
-    http
-        .formLogin(AbstractHttpConfigurer::disable)
-        .httpBasic(AbstractHttpConfigurer::disable)
-        .csrf(AbstractHttpConfigurer::disable)
-        .headers(
-            (headers) ->
-                headers
-                    .contentTypeOptions(Customizer.withDefaults())
-                    .xssProtection(Customizer.withDefaults())
-                    .cacheControl(Customizer.withDefaults())
-                    .httpStrictTransportSecurity(Customizer.withDefaults())
-                    .frameOptions(Customizer.withDefaults())
-                    .disable())
-        .authorizeHttpRequests(
-            authorize ->
-                authorize
-                    .requestMatchers("/h2/**", "/h2-console/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-        )
-        .sessionManagement(session ->
-                               session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+      http.addFilterBefore(jwtAuthenticationFilter,
+          UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
+      return http.build();
+
+    } catch (Exception ex) {
+      
+      logger.error(ex.getMessage());
+      logger.error(Arrays.toString(ex.getStackTrace()));
+
+      throw new InternalServerError(ExceptionDefinition.INT0001);
+    }
   }
 }
