26851f12e5dc0d06c5ac1a4a7685895413c9ae1c
Joao Chaves <joao.chaves@liferay.com>
2024-07-15 14:09:52 -0300

    rename it to base role request dto and apply to usages

--- application/src/main/java/com/cleanarch/application/gateway/role/UpdateRoleGateway.java
+++ application/src/main/java/com/cleanarch/application/gateway/role/UpdateRoleGateway.java
@@ -1,12 +1,12 @@
 package com.cleanarch.application.gateway.role;
 
-import com.cleanarch.usecase.common.dto.MessageResponseDTO;
-import com.cleanarch.usecase.role.dto.RoleRequestDTO;
+import com.cleanarch.usecase.common.dto.*;
+import com.cleanarch.usecase.role.dto.*;
 
 public interface UpdateRoleGateway {
 
   MessageResponseDTO update(
       Long roleId,
-      RoleRequestDTO requestDTO
+      BaseRoleRequestDTO requestDTO
   );
 }
