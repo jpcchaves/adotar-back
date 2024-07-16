33f54f37b87fd2b98c17c4be7819199110a089ae
João Chaves <joao.chaves@liferay.com>
2024-07-16 14:59:37 -0300

    add login service in auth service

--- infra/src/main/java/com/cleanarch/infra/service/auth/AuthService.java
+++ infra/src/main/java/com/cleanarch/infra/service/auth/AuthService.java
@@ -1,9 +1,13 @@
 package com.cleanarch.infra.service.auth;
 
+import com.cleanarch.infra.domain.dto.auth.LoginRequestDTO;
 import com.cleanarch.infra.domain.dto.auth.RegisterRequestDTO;
+import com.cleanarch.usecase.auth.dto.LoginResponseDTO;
 import com.cleanarch.usecase.common.dto.MessageResponseDTO;
 
 public interface AuthService {
 
   MessageResponseDTO register(RegisterRequestDTO requestDTO);
+
+  LoginResponseDTO login(LoginRequestDTO requestDTO);
 }
--- infra/src/main/java/com/cleanarch/infra/service/auth/impl/AuthServiceImpl.java
+++ infra/src/main/java/com/cleanarch/infra/service/auth/impl/AuthServiceImpl.java
@@ -1,8 +1,11 @@
 package com.cleanarch.infra.service.auth.impl;
 
+import com.cleanarch.infra.domain.dto.auth.LoginRequestDTO;
 import com.cleanarch.infra.domain.dto.auth.RegisterRequestDTO;
 import com.cleanarch.infra.service.auth.AuthService;
+import com.cleanarch.usecase.auth.LoginUseCase;
 import com.cleanarch.usecase.auth.RegisterUseCase;
+import com.cleanarch.usecase.auth.dto.LoginResponseDTO;
 import com.cleanarch.usecase.common.dto.MessageResponseDTO;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,23 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
   private final RegisterUseCase registerUseCase;
+  private final LoginUseCase loginUseCase;
 
-  public AuthServiceImpl(RegisterUseCase registerUseCase) {
+  public AuthServiceImpl(
+      RegisterUseCase registerUseCase,
+      LoginUseCase loginUseCase
+  ) {
     this.registerUseCase = registerUseCase;
+    this.loginUseCase = loginUseCase;
   }
 
   @Override
   public MessageResponseDTO register(RegisterRequestDTO requestDTO) {
     return new MessageResponseDTO(registerUseCase.register(requestDTO));
+  }
+
+  @Override
+  public LoginResponseDTO login(LoginRequestDTO requestDTO) {
+    return loginUseCase.login(requestDTO);
   }
 }
