45a25304f4e827544c4d84218433a68dfdd77249
João Chaves <joao.chaves@liferay.com>
2024-07-19 22:54:00 -0300

    add password matches annotation

--- infra/src/main/java/com/cleanarch/infra/domain/dto/auth/RegisterRequestDTO.java
+++ infra/src/main/java/com/cleanarch/infra/domain/dto/auth/RegisterRequestDTO.java
@@ -1,10 +1,12 @@
 package com.cleanarch.infra.domain.dto.auth;
 
+import com.cleanarch.infra.domain.annotation.pwd.PasswordMatches;
 import com.cleanarch.usecase.auth.dto.BaseRegisterRequestDTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
+@PasswordMatches(message = "The password and it's confirmation does not match!")
 public class RegisterRequestDTO extends BaseRegisterRequestDTO {
 
   @Override
@@ -26,7 +28,7 @@ public class RegisterRequestDTO extends BaseRegisterRequestDTO {
   public String getPassword() {
     return super.getPassword();
   }
-  
+
   @Override
   @NotBlank(message = "A confirmacao de senha e obrigatoria!")
   @Length(min = 8, message = "A senha deve conter 8 caracteres ou mais")
