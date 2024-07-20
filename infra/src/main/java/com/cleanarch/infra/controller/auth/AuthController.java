8b9b078f8726231a9c62d3057badc89104ba5dee
João Chaves <joao.chaves@liferay.com>
2024-07-19 23:02:55 -0300

    add update password endpoint

--- infra/src/main/java/com/cleanarch/infra/controller/auth/AuthController.java
+++ infra/src/main/java/com/cleanarch/infra/controller/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.cleanarch.infra.controller.auth;
 
 import com.cleanarch.infra.domain.dto.auth.LoginRequestDTO;
 import com.cleanarch.infra.domain.dto.auth.RegisterRequestDTO;
+import com.cleanarch.infra.domain.dto.auth.UpdatePasswordDTO;
 import com.cleanarch.infra.service.auth.AuthService;
 import com.cleanarch.usecase.auth.dto.LoginResponseDTO;
 import com.cleanarch.usecase.common.dto.MessageResponseDTO;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,7 @@ public class AuthController {
       @RequestBody @Valid
       LoginRequestDTO requestDTO
   ) {
-    
+
     return ResponseEntity.ok(authService.login(requestDTO));
   }
 
@@ -41,6 +43,15 @@ public class AuthController {
   ) {
 
     return ResponseEntity.status(HttpStatus.CREATED)
-                         .body(authService.register(requestDTO));
+        .body(authService.register(requestDTO));
+  }
+
+  @PutMapping("/update-password")
+  public ResponseEntity<MessageResponseDTO> updatePassword(
+      @RequestBody @Valid
+      UpdatePasswordDTO
+          requestDTO
+  ) {
+    return ResponseEntity.ok(authService.updatePassword(requestDTO));
   }
 }
