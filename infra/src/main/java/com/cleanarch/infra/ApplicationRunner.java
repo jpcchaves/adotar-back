9673682212e4e4f1f6d45c12957eaf5a1a7e5e7f
Joao Chaves <=>
2024-07-28 21:48:18 -0300

    add enable async and transaction management

--- infra/src/main/java/com/cleanarch/infra/ApplicationRunner.java
+++ infra/src/main/java/com/cleanarch/infra/ApplicationRunner.java
@@ -2,8 +2,12 @@ package com.cleanarch.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAsync
 @SpringBootApplication
+@EnableTransactionManagement
 public class ApplicationRunner {
 
   public static void main(String[] args) {
