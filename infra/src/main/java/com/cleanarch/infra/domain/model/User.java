a2091e3e51cd3e5b2876402dc8a5d84d0ae187c9
jpcchaves <joao.chaves@liferay.com>
2024-08-04 10:30:54 -0300

    add constructor

--- infra/src/main/java/com/cleanarch/infra/domain/model/User.java
+++ infra/src/main/java/com/cleanarch/infra/domain/model/User.java
@@ -128,6 +128,25 @@ public class User implements UserDetails, Serializable {
     this.roles = roles;
   }
 
+  public User(
+      Long id,
+      String firstName,
+      String lastName,
+      String email,
+      String password,
+      Boolean isActive,
+      Date createdAt,
+      Date lastSeen) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.isActive = isActive;
+    this.createdAt = createdAt;
+    this.lastSeen = lastSeen;
+  }
+
   public Long getId() {
     return id;
   }
