e4a17655ef3ca8b0e491316f0591446204b42431
jpcchaves <jpcchaves@outlook.com>
2024-08-15 12:42:59 -0300

    adjust mail service to be use test containers

--- infra/src/test/java/com/cleanarch/infra/service/mail/impl/MailServiceImplTest.java
+++ infra/src/test/java/com/cleanarch/infra/service/mail/impl/MailServiceImplTest.java
@@ -3,18 +3,21 @@ package com.cleanarch.infra.service.mail.impl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
+import com.cleanarch.infra.config.testcontainer.AbstractTestContainerConfig;
 import com.cleanarch.infra.service.mail.MailService;
 import com.cleanarch.infra.service.mail.MailTemplates;
 import java.util.concurrent.CompletableFuture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = {"classpath:.env"})
-class MailServiceImplTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MailServiceImplTest extends AbstractTestContainerConfig {
 
   @Autowired private MailService mailService;
 
@@ -29,17 +32,14 @@ class MailServiceImplTest {
 
     // When / Act
     mailService.send(
-        "Welcome to Adotar!",
-        mailTemplates.getRegisterSuccessfulTemplate("Joao"),
-        recipient);
+        "Welcome to Adotar!", mailTemplates.getRegisterSuccessfulTemplate("Joao"), recipient);
 
     // Then / Assert
   }
 
   @DisplayName("Test send email with HTML body using CompletableFuture")
   @Test
-  void
-      testEmailSend_When_SendingEmailWithCompletableFuture_ShouldArriveToRecipient() {
+  void testEmailSend_When_SendingEmailWithCompletableFuture_ShouldArriveToRecipient() {
 
     // Given
     String recipient = "jpcchaves@outlook.com";
