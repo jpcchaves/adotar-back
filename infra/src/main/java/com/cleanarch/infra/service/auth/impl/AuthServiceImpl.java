6fe984b3b88cf38246755e9a60f2acf423109b81
jpcchaves <jpcchaves@outlook.com>
2024-08-17 11:34:40 -0300

    implement service update user

--- infra/src/main/java/com/cleanarch/infra/service/auth/impl/AuthServiceImpl.java
+++ infra/src/main/java/com/cleanarch/infra/service/auth/impl/AuthServiceImpl.java
@@ -3,11 +3,13 @@ package com.cleanarch.infra.service.auth.impl;
 import com.cleanarch.infra.domain.dto.auth.LoginRequestDTO;
 import com.cleanarch.infra.domain.dto.auth.RegisterRequestDTO;
 import com.cleanarch.infra.domain.dto.auth.UpdatePasswordDTO;
+import com.cleanarch.infra.domain.dto.auth.UpdateUserRequestDTO;
 import com.cleanarch.infra.factory.common.ConcreteMessageResponseFactory;
 import com.cleanarch.infra.service.auth.AuthService;
 import com.cleanarch.usecase.auth.LoginUseCase;
 import com.cleanarch.usecase.auth.RegisterUseCase;
 import com.cleanarch.usecase.auth.UpdatePasswordUseCase;
+import com.cleanarch.usecase.auth.UpdateUserUseCase;
 import com.cleanarch.usecase.auth.dto.LoginResponseDTO;
 import com.cleanarch.usecase.common.dto.MessageResponseDTO;
 import org.springframework.stereotype.Service;
@@ -18,14 +20,17 @@ public class AuthServiceImpl implements AuthService {
   private final RegisterUseCase registerUseCase;
   private final LoginUseCase loginUseCase;
   private final UpdatePasswordUseCase updatePasswordUseCase;
+  private final UpdateUserUseCase updateUserUseCase;
 
   public AuthServiceImpl(
       RegisterUseCase registerUseCase,
       LoginUseCase loginUseCase,
-      UpdatePasswordUseCase updatePasswordUseCase) {
+      UpdatePasswordUseCase updatePasswordUseCase,
+      UpdateUserUseCase updateUserUseCase) {
     this.registerUseCase = registerUseCase;
     this.loginUseCase = loginUseCase;
     this.updatePasswordUseCase = updatePasswordUseCase;
+    this.updateUserUseCase = updateUserUseCase;
   }
 
   @Override
@@ -42,5 +47,10 @@ public class AuthServiceImpl implements AuthService {
   @Override
   public MessageResponseDTO updatePassword(UpdatePasswordDTO requestDTO) {
     return updatePasswordUseCase.updatePassword(requestDTO);
+  }
+
+  @Override
+  public MessageResponseDTO updateUser(UpdateUserRequestDTO requestDTO) {
+    return updateUserUseCase.updateUser(requestDTO);
   }
 }
