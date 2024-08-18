b49e7b808cd13e66b99982d83e5c330c4ada8a6e
jpcchaves <jpcchaves@outlook.com>
2024-08-18 14:41:31 -0300

    add test find all by user

---
+++ infra/src/test/java/com/cleanarch/infra/repository/UserFavoritePetsRepositoryTest.java
@@ -0,0 +1,88 @@
+package com.cleanarch.infra.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.com.jpcchaves.core.domain.enums.AnimalSize;
+import br.com.jpcchaves.core.domain.enums.AnimalType;
+import br.com.jpcchaves.core.domain.enums.Gender;
+import br.com.jpcchaves.core.domain.enums.HealthCondition;
+import com.cleanarch.infra.config.testcontainer.AbstractTestContainerConfig;
+import com.cleanarch.infra.domain.model.Pet;
+import com.cleanarch.infra.domain.model.User;
+import com.cleanarch.infra.domain.model.UserFavoritePets;
+import java.util.List;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@TestPropertySource(
+    locations = {"classpath:application-test.yml", "classpath:.env"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserFavoritePetsRepositoryTest extends AbstractTestContainerConfig {
+
+  private User user;
+  private Pet pet;
+
+  private Faker faker;
+
+  @Autowired private PetRepository petRepository;
+  @Autowired private UserRepository userRepository;
+  @Autowired private UserFavoritePetsRepository userFavoritePetsRepository;
+
+  @BeforeEach
+  void setup() {
+
+    faker = new Faker();
+
+    user =
+        userRepository.save(
+            new User(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.internet().emailAddress(),
+                faker.internet().password(),
+                Boolean.TRUE));
+
+    pet =
+        petRepository.save(
+            new Pet(
+                faker.dog().name(),
+                1,
+                2,
+                Gender.MALE,
+                AnimalSize.LARGE,
+                HealthCondition.HEALTHY,
+                AnimalType.DOG,
+                faker.color().name(),
+                faker.text().text(20),
+                faker.dog().breed(),
+                user));
+  }
+
+  @DisplayName(
+      "Test given User when find all by User then should return Pets created by"
+          + " the  user")
+  @Test
+  void testGivenUser_WhenFindAllByUser_ThenShouldReturnPetsCreatedByTheUser() {
+
+    // Given / Arrange
+    userFavoritePetsRepository.save(new UserFavoritePets(user, pet));
+
+    // When / Act
+    List<UserFavoritePets> userFavoritePetsList =
+        userFavoritePetsRepository.findAllByUser(user.getId());
+
+    // Then / Assert
+    assertNotNull(userFavoritePetsList);
+
+    assertEquals(1, userFavoritePetsList.size());
+
+    assertEquals(user.getId(), userFavoritePetsList.get(0).getUser().getId());
+  }
+}
