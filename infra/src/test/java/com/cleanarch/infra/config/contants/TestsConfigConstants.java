57bdd1b6f5aa5a25c48b66e70b0ba99da5d03b5d
jpcchaves <jpcchaves@outlook.com>
2024-08-19 12:36:42 -0300

    rename content type constant also add email test

--- infra/src/test/java/com/cleanarch/infra/config/contants/TestsConfigConstants.java
+++ infra/src/test/java/com/cleanarch/infra/config/contants/TestsConfigConstants.java
@@ -4,5 +4,7 @@ public class TestsConfigConstants {
 
   public static final int SERVER_PORT = 8888;
 
-  public static final String CONTENT_TYPE = "application/json";
+  public static final String CONTENT_TYPE_JSON = "application/json";
+
+  public static final String EMAIL_TEST = "testsadotar@outlook.com";
 }
