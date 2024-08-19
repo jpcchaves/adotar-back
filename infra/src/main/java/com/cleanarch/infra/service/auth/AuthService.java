f3cb7c7e5e91e6d34e9b13abad577531a6427465
jpcchaves <jpcchaves@outlook.com>
2024-08-19 10:14:58 -0300

    add auth service reset token request method

--- infra/src/main/java/com/cleanarch/infra/service/auth/AuthService.java
+++ infra/src/main/java/com/cleanarch/infra/service/auth/AuthService.java
@@ -1,9 +1,6 @@
 package com.cleanarch.infra.service.auth;
 
-import com.cleanarch.infra.domain.dto.auth.LoginRequestDTO;
-import com.cleanarch.infra.domain.dto.auth.RegisterRequestDTO;
-import com.cleanarch.infra.domain.dto.auth.UpdatePasswordDTO;
-import com.cleanarch.infra.domain.dto.auth.UpdateUserRequestDTO;
+import com.cleanarch.infra.domain.dto.auth.*;
 import com.cleanarch.usecase.auth.dto.LoginResponseDTO;
 import com.cleanarch.usecase.common.dto.MessageResponseDTO;
 
@@ -16,4 +13,6 @@ public interface AuthService {
   MessageResponseDTO updatePassword(UpdatePasswordDTO requestDTO);
 
   MessageResponseDTO updateUser(UpdateUserRequestDTO requestDTO);
+
+  MessageResponseDTO resetTokenRequest(PasswordResetRequestDTO requestDTO);
 }
